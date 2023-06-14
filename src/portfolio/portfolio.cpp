0d088f9b731a41f9c834bf669249c532db56caf8
omar <omarkhaledsamy20@gmail.com>
2023-06-14 17:59:14 +0200

    add investment class and implement portfolio class

--- src/portfolio/portfolio.cpp
+++ src/portfolio/portfolio.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
 #include "portfolio.hpp"
+
+void Portfolio::addInvestment(const Investment& investment) {
+    m_investments.push_back(investment);
+}
+
+void Portfolio::removeInvestment(const std::string& ticker) {
+    for (auto it = m_investments.begin(); it != m_investments.end(); ++it) {
+        if (it->getTicker() == ticker) {
+            m_investments.erase(it);
+            break;
+        }
+    }
+}
+
+void Portfolio::displayPortfolio() const {
+    std::cout << "Portfolio: " << m_name << std::endl;
+    for (const auto& investment : m_investments) {
+        std::cout << "Name: " << investment.getName() << ", Ticker: "
+                  << investment.getTicker() << ", Quantity: "
+                  << investment.getQuantity() << ", Purchase price: "
+                  << investment.getPurchasePrice()
+                  << std::endl;
+    }
+}
+
+double_t Portfolio::calculateTotalValue() const {
+    double_t totalValue = 0.0;
+    for (const auto& investment : m_investments) {
+        totalValue += investment.getPurchasePrice() * investment.getQuantity();
+    }
+    return totalValue;
+}
+
+void Portfolio::updatedInvestmentValue(const std::string& ticker, double_t newValue) {
+    for (auto& investment : m_investments) {
+        if (investment.getTicker() == ticker) {
+            investment.setPurchasePrice(newValue);
+            break;
+        }
+    }
+}
--- src/portfolio/portfolio.hpp
+++ src/portfolio/portfolio.hpp
@@ -1,15 +1,23 @@
 #ifndef PORTFOLIO_HPP_
 #define PORTFOLIO_HPP_
 
+#include <string>
+#include <vector>
+#include "investment.hpp"
+
 class Portfolio {
+private:
+    std::string m_name;
+    std::vector<Investment> m_investments;
 
 public:
-    Portfolio() {}
-    ~Portfolio() {}
+    Portfolio(const std::string& name) : m_name(name) {}
 
-private:
-    void updatePortfolio();
-    bool isValidPortfolio() const;
+    void addInvestment(const Investment& investment);
+    void removeInvestment(const std::string& ticker);
+    void displayPortfolio() const;
+    double_t calculateTotalValue() const;
+    void updatedInvestmentValue(const std::string& ticker, double_t newValue);
 };
 
 
